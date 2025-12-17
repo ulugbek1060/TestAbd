b2ebd512d63f86cf783a45aac915d2999c485772
ulugbek1060 <ulugg1060@gmail.com>
2025-12-18 00:31:58 +0500

    building

--- lib/features/root/shell_screen.dart
+++ lib/features/root/shell_screen.dart
@@ -19,9 +19,9 @@ class ShellScreen extends StatelessWidget {
       bottomNavigationBar: BottomNavigationBar(
         elevation: 0,
         backgroundColor: Colors.transparent,
-        unselectedIconTheme: const IconThemeData(color: Colors.black),
+        unselectedIconTheme: const IconThemeData(color: Colors.white),
         unselectedLabelStyle: const TextStyle(
-          color: Colors.black,
+          color: Colors.white,
           fontSize: 12,
         ),
         selectedIconTheme: IconThemeData(color: colorScheme.secondary),
@@ -45,34 +45,10 @@ class ShellScreen extends StatelessWidget {
         ],
         selectedItemColor: colorScheme.secondary,
         currentIndex: navShell.currentIndex,
-        unselectedItemColor: Colors.black,
+        unselectedItemColor: Colors.white,
         type: BottomNavigationBarType.fixed,
         onTap: (index) => navShell.goBranch(index),
       ),
     );
   }
-
-  //       bottomNavigationBar: BottomNavigationBar(
-//         backgroundColor: Colors.black,
-//         selectedItemColor: Colors.white,
-//         unselectedItemColor: Colors.grey,
-//         type: BottomNavigationBarType.fixed,
-//         items: const [
-//           BottomNavigationBarItem(
-//             icon: Icon(Icons.home_outlined),
-//             label: 'Home',
-//           ),
-//           BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Discover'),
-//           BottomNavigationBarItem(
-//             icon: Icon(Icons.add_circle_outline),
-//             label: 'Create',
-//           ),
-//           BottomNavigationBarItem(
-//             icon: Icon(Icons.notifications_outlined),
-//             label: 'Activity',
-//           ),
-//           BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
-//         ],
-//         currentIndex: 4,
-//       ),
 }
