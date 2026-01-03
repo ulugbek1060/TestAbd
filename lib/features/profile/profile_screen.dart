2d7bd47e47c8c2b51fa9284ad3589283db1bc4c0
ulugbek <ulugg1060@gmail.com>
2026-01-03 23:16:06 +0500

    building

--- lib/features/profile/profile_screen.dart
+++ lib/features/profile/profile_screen.dart
@@ -4,6 +4,8 @@ import 'package:testabd/di/app_config.dart';
 import 'package:testabd/features/profile/profile_cubit.dart';
 import 'package:testabd/features/profile/profile_state.dart';
 
+enum PageType { block, questions, books }
+
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
 
@@ -16,8 +18,29 @@ class ProfileScreen extends StatelessWidget {
   }
 }
 
-class _View extends StatelessWidget {
+class _View extends StatefulWidget {
   const _View();
+
+  @override
+  State<_View> createState() => _ViewState();
+}
+
+class _ViewState extends State<_View> with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+  late var pageTye = PageType.block;
+  late var _blockKey;
+  late var _questionsKey;
+  late var _booksKey;
+
+
+  @override
+  void initState() {
+    super.initState();
+    _blockKey = PageStorageKey('blocksSection');
+    _questionsKey = PageStorageKey('questionsSection');
+    _booksKey = PageStorageKey('booksSection');
+    _tabController = TabController(length: 3, vsync: this);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -43,11 +66,11 @@ class _View extends StatelessWidget {
               SizedBox(width: 12),
             ],
           ),
-          body: SingleChildScrollView(
-            child: Column(
-              children: [
-                /// Header
-                Container(
+          body: CustomScrollView(
+            slivers: [
+              /// Header
+              SliverToBoxAdapter(
+                child: Container(
                   padding: const EdgeInsets.all(16),
                   child: Row(
                     children: [
@@ -71,37 +94,42 @@ class _View extends StatelessWidget {
                     ],
                   ),
                 ),
+              ),
 
-                /// Bio
-                Container(
-                  width: double.infinity,
-                  padding: const EdgeInsets.symmetric(horizontal: 16),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "Beginner Developer 👨‍💻",
-                        style: TextStyle(
-                          color: Theme.of(context).colorScheme.onSurface,
-                          fontWeight: FontWeight.w600,
+              /// Bio
+              SliverPadding(
+                padding: EdgeInsets.only(bottom: 16),
+                sliver: SliverToBoxAdapter(
+                  child: Container(
+                    width: double.infinity,
+                    padding: const EdgeInsets.symmetric(horizontal: 16),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Beginner Developer 👨‍💻",
+                          style: TextStyle(
+                            color: Theme.of(context).colorScheme.onSurface,
+                            fontWeight: FontWeight.w600,
+                          ),
                         ),
-                      ),
 
-                      SizedBox(height: 4),
+                        SizedBox(height: 4),
 
-                      Text(
-                        "Learning • Practicing • Growing",
-                        style: TextStyle(color: Colors.grey),
-                      ),
-                    ],
+                        Text(
+                          "Learning • Practicing • Growing",
+                          style: TextStyle(color: Colors.grey),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
+              ),
 
-                const SizedBox(height: 16),
-
-                /// Buttons
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 16),
+              /// Buttons
+              SliverPadding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                sliver: SliverToBoxAdapter(
                   child: Row(
                     children: [
                       Expanded(
@@ -146,12 +174,54 @@ class _View extends StatelessWidget {
                     ],
                   ),
                 ),
+              ),
 
-                const SizedBox(height: 24),
+              /// Statistics Cards
+              SliverPadding(
+                padding: EdgeInsets.only(left: 16, right: 16, top: 16),
+                sliver: SliverToBoxAdapter(
+                  child: _PerformanceItem(
+                    title: "Statistics",
+                    value: "22",
+                    icon: Icons.eleven_mp,
+                    color: Colors.green,
+                    progress: 30,
+                  ),
+                ),
+              ),
 
-                /// Statistics Cards
-              ],
-            ),
+              /// tabBar
+              SliverPersistentHeader(
+                pinned: true,
+                delegate: _SliverAppBarDelegate(
+                  backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+                  TabBar(
+                    unselectedLabelColor: Theme.of(
+                      context,
+                    ).colorScheme.onSurface,
+                    labelColor: Theme.of(context).colorScheme.onSurface,
+                    onTap: (index) {
+                      setState(() {
+                        switch (index) {
+                          case 0:
+                            pageTye = PageType.block;
+                          case 1:
+                            pageTye = PageType.questions;
+                          case 2:
+                            pageTye = PageType.books;
+                        }
+                      });
+                    },
+                    controller: _tabController,
+                    tabs: [
+                      Tab(text: 'Bloklar'),
+                      Tab(text: 'Savollar'),
+                      Tab(text: 'Kitoblar'),
+                    ],
+                  ),
+                ),
+              ),
+            ],
           ),
         );
       },
@@ -185,6 +255,95 @@ class _StatItem extends StatelessWidget {
         ),
       ],
     );
+  }
+}
+
+class _PerformanceItem extends StatelessWidget {
+  final String title;
+  final String value;
+  final IconData icon;
+  final Color color;
+  final double? progress;
+
+  const _PerformanceItem({
+    required this.title,
+    required this.value,
+    required this.icon,
+    required this.color,
+    this.progress,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Theme.of(context).colorScheme.surface,
+        borderRadius: BorderRadius.circular(16),
+      ),
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Row(
+            children: [
+              Container(
+                padding: const EdgeInsets.all(8),
+                decoration: BoxDecoration(
+                  color: color.withAlpha(50),
+                  shape: BoxShape.circle,
+                ),
+                child: Icon(icon, color: color, size: 18),
+              ),
+              const Spacer(),
+              Text(
+                value,
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.onSurface,
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+          Text(title, style: TextStyle(color: Colors.grey, fontSize: 14)),
+          if (progress != null)
+            LinearProgressIndicator(
+              value: (progress ?? 1) / 100,
+              valueColor: AlwaysStoppedAnimation<Color>(Colors.green),
+              backgroundColor: Color(0xffD6D6D6),
+              borderRadius: BorderRadius.circular(4),
+            ),
+        ],
+      ),
+    );
+  }
+}
+
+class _SliverAppBarDelegate extends SliverPersistentHeaderDelegate {
+  final Color backgroundColor;
+
+  _SliverAppBarDelegate(this._tabBar, {required this.backgroundColor});
+
+  final TabBar _tabBar;
+
+  @override
+  double get minExtent => _tabBar.preferredSize.height;
+
+  @override
+  double get maxExtent => _tabBar.preferredSize.height;
+
+  @override
+  Widget build(
+    BuildContext context,
+    double shrinkOffset,
+    bool overlapsContent,
+  ) => Container(color: backgroundColor, child: _tabBar);
+
+  @override
+  bool shouldRebuild(_SliverAppBarDelegate oldDelegate) {
+    return true;
   }
 }
 
